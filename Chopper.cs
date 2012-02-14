47d4aaca0a0c3b13fcce0ff68d25ac1ffe136220
Jeff <jeff.vera@gmail.com>
2012-02-14 15:26:47 -0500

    Initial commit.

---
+++ Chopper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KarateChopKataOne
+{
+	public class Chopper
+	{
+		public int FindIndexOfTarget(
+			int numberToFind, 
+			List<int> listToSearch)
+		{			
+			if (listToSearch == null || listToSearch.Count == 0)
+			{
+				return -1;
+			}
+			else if (listToSearch.Count == 1)
+			{
+				if (numberToFind == listToSearch[0])
+				{
+					return 0;
+				}
+				else
+				{
+					return -1;
+				}
+			}
+			else
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void SplitListInHalf(
+			List<int> listToSplit, 
+			out List<int> topHalf, 
+			out List<int> bottomHalf)
+		{
+			if (listToSplit.Count % 2 == 0)
+			{
+				int halfwayPoint = listToSplit.Count / 2;
+				topHalf =
+					listToSplit.Take(halfwayPoint).ToList<int>();
+				bottomHalf =
+					listToSplit.Reverse<int>()
+					.Take(halfwayPoint).ToList<int>()
+					.Reverse<int>().ToList<int>()
+					;
+			}
+			else
+			{
+				int indexToSplitOn = 
+					(int)Math.Floor((double)listToSplit.Count / 2);
+				topHalf =
+					listToSplit.Take(indexToSplitOn).ToList<int>();
+				bottomHalf =
+					listToSplit.Reverse<int>()
+					.Take(indexToSplitOn + 1).ToList<int>()
+					.Reverse<int>().ToList<int>();
+			}
+		}
+	}
+}
---
+++ ChopperTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KarateChopKataOne
+{
+	[TestFixture]
+	public class ChopperTests
+	{
+		[Test]
+		public void CanInstantiateTest()
+		{
+			Chopper c = new Chopper();
+
+			Assert.That(c, Is.Not.Null);
+		}
+
+		[Test]
+		public void EmptyListAlwaysReturnsNegativeOneTest()
+		{
+			List<int> emptyList = new List<int>();
+			int numberToFind = 42;
+			Chopper c = new Chopper();
+
+			int returnedIndex = c.FindIndexOfTarget(numberToFind, emptyList);
+
+			Assert.That(returnedIndex, Is.EqualTo(-1));
+		}
+
+		[Test]
+		public void ListOfOneItemThatContainsInputReturnsIndexOfZeroTest()
+		{
+			List<int> oneItemList = new List<int> { 42 };
+			int numberToFind = 42;
+			Chopper c = new Chopper();
+
+			int returnedIndex = 
+				c.FindIndexOfTarget(numberToFind, oneItemList);
+
+			Assert.That(returnedIndex, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void ListOfOneItemThatDoesNotContainInputReturnsNegOneTest()
+		{
+			List<int> oneItemList = new List<int> { 42 };
+			int numberToFind = 41;
+			Chopper c = new Chopper();
+
+			int returnedIndex =
+				c.FindIndexOfTarget(numberToFind, oneItemList);
+
+			Assert.That(returnedIndex, Is.EqualTo(-1));
+		}
+
+		[Test]
+		public void CanSplitEvenNumberedListInHalfTest()
+		{
+			List<int> evenNumberedList = new List<int> { 2, 4, 6, 8 };
+			List<int> topHalf = null;
+			List<int> bottomHalf = null;
+			Chopper c = new Chopper();
+
+			c.SplitListInHalf(
+				evenNumberedList, 
+				out topHalf, 
+				out bottomHalf);
+
+			Assert.That(topHalf, Is.Not.Null);
+			Assert.That(topHalf.Count, Is.EqualTo(2));
+			Assert.That(topHalf, 
+				Is.EqualTo(new List<int> { 2, 4 }));
+			Assert.That(bottomHalf, Is.Not.Null);
+			Assert.That(bottomHalf.Count, Is.EqualTo(2));
+			Assert.That(bottomHalf, 
+				Is.EqualTo(new List<int> { 6, 8 }));
+		}
+
+		[Test]
+		public void CanSplitOddNumberedListInHalfWithTopHavingOneFewerTest()
+		{
+			List<int> oddNumberedList = new List<int> { 2, 4, 6 };
+			List<int> topHalf = null;
+			List<int> bottomHalf = null;
+			Chopper c = new Chopper();
+
+			c.SplitListInHalf(
+				oddNumberedList,
+				out topHalf,
+				out bottomHalf);
+
+			Assert.That(topHalf, Is.Not.Null);
+			Assert.That(topHalf.Count, Is.EqualTo(1));
+			Assert.That(topHalf[0], Is.EqualTo(2));
+			Assert.That(bottomHalf, Is.Not.Null);
+			Assert.That(bottomHalf.Count, Is.EqualTo(2));
+			Assert.That(bottomHalf, 
+				Is.EqualTo(new List<int> { 4, 6 }));
+		}
+	}
+}
